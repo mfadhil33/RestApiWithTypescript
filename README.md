8fb52593f1085b7e57c70f82cfb46334adc52465
Mhd Fadhil Arya Putra <fadhilgaming522@gmail.com>
2023-02-13 22:38:26 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# RestApi with using Ts
+
+***RestApi using ts [Baileys](https://github.com/adiwajshing/Baileys)***
+
+## Install
+**Install on Linux ✅**
+
+Install [NodeJS](https://nodejs.org/en/download/)
+terlebih dahulu
+$ git clone https://github.com/mfadhil33/RestApiWithTypescript
+$ cd RestApiWithTypescript
+$ npm install
+$ npm run dev
+```
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import  express , {Application, Request, Response, NextFunction} from 'express';
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+dotenv.config();
+
+const app: Application = express();
+const port: Number | undefined | string  = process.env.PORT;
+const host : string | undefined = process.env.HOST;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use("/", (req: Request, res: Response, nex: NextFunction) => {
+
+    res.status(200).send({data: "Hello world"});
+});
+
+app.listen(port, () => {
+ console.log(`server runnning on port http://${host}:${port}`);
+});
