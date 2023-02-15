ef74aa5dbb5ee8db791c781ec7342f5808ab615b
Mhd Fadhil Arya Putra <fadhilgaming522@gmail.com>
2023-02-15 09:38:52 +0400

    repair style coding in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
-import  express , {Application, Request, Response, NextFunction} from 'express';
-import dotenv from 'dotenv';
-import bodyParser from "body-parser";
-dotenv.config();
+import express, { type Application, type Request, type Response, type NextFunction } from 'express'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+dotenv.config()
 
-const app: Application = express();
-const port: Number | undefined | string  = process.env.PORT;
-const host : string | undefined = process.env.HOST;
+const app: Application = express()
+const port: number | undefined | string = process.env.PORT
+const host: string | undefined = process.env.HOST
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use('/', (req: Request, res: Response, nex: NextFunction) => {
-
-    res.status(200).send({data: 'Hello world'});
-});
+  res.status(200).send({ data: 'Hello world' })
+})
 
 app.listen(port, () => {
- console.log(`server runnning on  http://${host}:${port}`);
-});
+  console.log(`server runnning on  http://${host}:${port}`)
+})
